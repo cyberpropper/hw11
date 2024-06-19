93583be6f11830e558aaa9cdccff920a4aa2b76a
cyberpropper <osankin2002@mail.ru>
2024-06-19 20:11:24 +0700

    2

--- src/Main.java
+++ src/Main.java
@@ -13,49 +13,4 @@ public class Main {
     }
 }
 
-class Author {
-    private String firstName;
-    private String lastName;
-
-    public Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-}
-
-class Book {
-    private String title;
-    private Author author;
-    private int publicationYear;
-
-    public Book(String title, Author author, int publicationYear) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-}
 
